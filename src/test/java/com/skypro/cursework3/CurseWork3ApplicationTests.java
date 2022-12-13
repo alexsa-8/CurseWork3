815851d804fab93bb6dc041c4e9b1a23f05d1bb3
Alex <alexsa-8@mail.ru>
2022-12-13 21:52:35 +0300

    Курсовая работа 3

---
+++ src/test/java/com/skypro/cursework3/CurseWork3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.cursework3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurseWork3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
